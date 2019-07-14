4ef5b131520eb0b4dd7176d510426b878ff07d24
Joshua Maxcy <kanemaxcy@gmail.com>
2019-07-13 22:14:53 -0500

    linear scaling for heights

--- buildings/js/main.js
+++ buildings/js/main.js
@@ -7,12 +7,16 @@ d3.json('data/buildings.json').then(data => {
     let rects = svg.selectAll('rect')
         .data(data)
     
+    const y = d3.scaleLinear()
+        .domain([0, 828])
+        .range([0, 400])
+
     rects.enter()
         .append('rect')
             .attr('x', (elm, idx) => (idx * 50) + 25)
             .attr('y', 25)
             .attr('width', 30)
-            .attr('height', elm => elm.height)
+            .attr('height', elm => y(elm.height))
             .attr('fill', 'grey')
 
 }).catch(error => console.log(error));
